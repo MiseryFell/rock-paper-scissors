8dd0ecaec7b24d5c477b3dcab84576d146a042bd
StanVreeland <stanvreeland@live.com>
2023-01-21 14:31:49 -0500

    completed project barring responsive design

--- script.js
+++ script.js
@@ -38,6 +38,7 @@ const paperReference = document.getElementById('paper');
 const scissorsReference = document.getElementById('scissors');
 const gameWrapper = document.getElementById('game-wrapper');
 const body = document.getElementById('parent');
+const computerWrapper = document.getElementById('computer-wrapper');
 
 // 
 
@@ -61,6 +62,15 @@ rockReference.addEventListener('click', () => {
     } else if (outcome[0] == 2) {
         computerWins++;
     }
+
+    if (outcome[2] === 'rock') {
+        computerText.textContent = "Computer Output: Rock"
+    }else if(outcome[2] === 'paper') {
+        computerText.textContent = "Computer Output: Paper"
+    }else if(outcome[2] === 'scissors') {
+        computerText.textContent = "Computer Output: Scissors"
+    }
+
     console.log(playerWins);
     console.log(computerWins);
     checkWins();
@@ -76,6 +86,15 @@ paperReference.addEventListener('click', () => {
     } else if (outcome[0] == 2) {
         computerWins++;
     }
+
+    if (outcome[2] === 'rock') {
+        computerText.textContent = "Computer Output: Rock"
+    }else if(outcome[2] === 'paper') {
+        computerText.textContent = "Computer Output: Paper"
+    }else if(outcome[2] === 'scissors') {
+        computerText.textContent = "Computer Output: Scissors"
+    }
+
     console.log(playerWins);
     console.log(computerWins);
     checkWins();
@@ -91,6 +110,15 @@ scissorsReference.addEventListener('click', () => {
     } else if (outcome[0] == 2) {
         computerWins++;
     }
+
+    if (outcome[2] === 'rock') {
+        computerText.textContent = "Computer Output: Rock"
+    }else if(outcome[2] === 'paper') {
+        computerText.textContent = "Computer Output: Paper"
+    }else if(outcome[2] === 'scissors') {
+        computerText.textContent = "Computer Output: Scissors"
+    }
+
     console.log(playerWins);
     console.log(computerWins);
     checkWins();
@@ -105,23 +133,28 @@ const textBox = document.createElement('div');
 const mainBoxText = document.createElement('div');
 const boxButtons = document.createElement('div');
 const restartButton = document.createElement('btn');
-const endButton = document.createElement('btn');
+// const endButton = document.createElement('btn');
+const computerText = document.createElement('p');
 
 endingWrapper.setAttribute('class', 'wrapper');
+endingWrapper.setAttribute('id', 'ending-wrapper');
 textBox.setAttribute('class', 'text-box');
 mainBoxText.setAttribute('id', 'main-box-text');
 boxButtons.setAttribute('id', 'text-box-buttons');
 restartButton.setAttribute('class', 'text-box-button');
-endButton.setAttribute('class', 'text-box-button');
+// endButton.setAttribute('class', 'text-box-button');
+computerText.setAttribute('id', 'computer-text');
 
 endingWrapper.appendChild(textBox);
 textBox.appendChild(mainBoxText);
 textBox.appendChild(boxButtons);
 boxButtons.appendChild(restartButton);
-boxButtons.appendChild(endButton);
+// boxButtons.appendChild(endButton);
+computerWrapper.appendChild(computerText);
 
 restartButton.textContent = "Restart?";
-endButton.textContent = "I give up...";
+// endButton.textContent = "I give up...";
+computerText.textContent = "Computer Output: Waiting";
 
 
 
@@ -139,12 +172,26 @@ function checkWins() {
 
 
 restartButton.addEventListener('click', () => {
-    console.log('nice');
+    console.log('restart click');
+    playerWins = 0;
+    computerWins = 0;
+    endingWrapper.remove()
+    body.appendChild(gameWrapper);
+    body.appendChild(computerWrapper);
 });
+
+
+/*
+endButton.addEventListener('click', () => {
+    console.log('end button click');
+    window.open('','_self').close();
+});
+*/
 
 
 function outcomeWin(winner='null') {
     gameWrapper.remove();
+    computerWrapper.remove();
     // body.removeChild(gameWrapper);
     let msToWait = 2000;
     setTimeout(() => {
@@ -152,5 +199,6 @@ function outcomeWin(winner='null') {
     }, msToWait);
     body.appendChild(endingWrapper);
     mainBoxText.textContent = `${winner} wins!`;
+    computerText.textContent = "Computer Output: Waiting";
 }
 
